6e9bef4bd4a9d274c75c6a1117c63ffd5e2a144c
S Kalpana <kalpanasathish2@gmail.com>
2017-06-20 09:21:12 +0530

    reverse.java

---
+++ reverse.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class reverse
+{
+public static void main(String args[])
+{
+String s=new String(args[0]);
+StringBuffer sb=new StringBuffer(s);
+sb.reverse();
+System.out.println(sb);
+}
+}
